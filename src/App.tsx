4c44ca2c7d14104492937e43bf0c6d8a43398813
Travis Holt <tholt@flogistix.com>
2021-03-29 10:02:23 -0500

    remove trash side bar

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,8 @@ const { Header } = Layout;
 const App = () => {
   return (
     <Layout style={{ height: '100vh' }}>
-      <Header />
-      <div style={{ background: 'white', borderRight: '1px solid black', position: 'fixed', height: '100vh', width: '72px', marginTop: '64px' }}>
-        ICONS
-      </div>
-      <div style={{ marginLeft: '72px' }}>
-        <FormGenerator />
-      </div>
+      <Header style={{ zIndex: 2}} />
+      <FormGenerator />
     </Layout>
   )
 };
